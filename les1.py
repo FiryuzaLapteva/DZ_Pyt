ab88cd7942e1aaec894ad6cbf0f891d1eef5068e
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-10-06 23:10:31 +0500

    начало изучения Python

---
+++ les1.py
@@ -0,0 +1,40 @@
+#print('Hello,world!')
+#value = None
+# a = 456
+# b = 12.5
+# print(type(a))
+# print(type(b))
+# print(type(value))
+
+# s = "'привет, манагер'"
+# st = '\'привет, манагер\''
+# print(s, '-',st)
+# print('{} - {}'.format(s, st))
+# print(f'{s} - {st}')
+
+# f = True
+# print(f)
+# list = ['1', '2', '3', 'hello', 1, 1.5, True]
+# print (list)
+
+# print('Введите а');
+# a = int(input()) #чтобы получить целое число int
+# print('Введите b');
+# b = int(input())
+# print (a, '+', b, '=', a+b)
+# print('{} {}'.format(a,b))
+# print(f'{a} {b}')
+
+# !!!Арифметические операции!!!
+
+# a = 2
+# b = 3
+# c = a**b # ** - возведение в степень, // - деление с округлением до целого
+# print(c)
+
+
+
+a = 2.1586124
+b = 3
+c = round (a * b,5) # rount -
+print (c)
