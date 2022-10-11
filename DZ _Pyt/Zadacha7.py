8ecca5753ed4d14b7f370c93f4a8942c86c5777c
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-10-12 00:17:09 +0500

    Семинар 2

--- DZ _Pyt/Zadacha7.py
+++ DZ _Pyt/Zadacha7.py
@@ -2,10 +2,11 @@
 # проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z 
 # для всех значений предикат.
 
-X = 1
-Y = 1
-Z = 1
-A = not(X or Y or Z)
-B = not X and not Y and not Z
-F = A == B
-print(F)
+X = [0,1]
+for i in X:
+    for j in X:
+        for k in X:
+            A = not(i or j or k)
+            B = not i and not j and not k
+            F = A == B
+            print(F)
