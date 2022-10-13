6281d9adb487152c449441f794ab458252d0060c
Firyuza Lapteva <Gen.chel.74@gmail.com>
2022-10-13 23:38:59 +0500

    done

---
+++ DZ _Pyt/Zadacha17.py
@@ -0,0 +1,13 @@
+# 17. Задайте список из N элементов, заполненных числами из промежутка [-N, N].
+#  Найдите произведение элементов на введенных пользователем позициях.
+
+number = int(input ('Введите целое число  '))
+my_list = []
+for i in range(- number,number +1):
+    x = i
+    my_list.append(x)
+print(my_list)
+
+def user(pos1: int, pos2: int, ):
+    for i in user:
+   #Не закончила
