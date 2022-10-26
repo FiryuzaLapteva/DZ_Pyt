180542c87789ea8d573b921f2abd50462c7dd84d
Firyuza_Lapteva <Gen.chel.74@gmail.com>
2022-10-26 15:28:28 +0500

    26/10/22

--- DZ _Pyt/Zadacha_Les_4.py
+++ DZ _Pyt/Zadacha_Les_4.py
@@ -13,29 +13,105 @@
 # горные лыжи
 
 
-keys = open('user.txt').read().split('\n')
-values = open('hobby.txt').read().split('\n')
-info = dict(zip(keys, values))
-print(info)
+# keys = open('DZ _Pyt/user.txt').read().split('\n')
+# values = open('DZ _Pyt/hobby.txt').read().split('\n')
+# info = dict(zip(keys, values))
+# print(info)
 
-import json
-open('user_hobby.txt', 'w').write(json.dumps(info))
-
-
+# import json
+# open('DZ _Pyt/users_hobby.txt', 'w').write(json.dumps(info))
 
 
+# 31. Задайте натуральное число N. Напишите программу, которая составит 
+# список простых множителей числа N.
 
 
+# N = int(input('Введите целое положительное число '))
+# def factor(num):
+#     my_list = list()
+#     m=2
+#     while(m<=num):
+#         if (num%m) == 0:
+#             my_list.append(m)
+#             num = num/m
+#         else:
+#             m +=1
+#     return my_list
+          
+# print(factor(N))      
+
+# 32. Задайте последовательность чисел. Напишите программу, 
+# которая выведет список неповторяющихся элементов исходной последовательности.
+
+# from random import randint, random
+# number = int(input('Введите размер списка '))
+# list = []
+# list2 = []
+
+# for i in range(number):
+#     list.append(randint(0, 10))# заполняем список list рандомно числами от 0 до 10
+# print(f'Исходная последовательность чисел {list}')
+
+# list2 = []
+# for i in list:
+#     if list.count(i)==1:
+#         list2.append(i)
+# print(list2)
 
 
-
-
-# 31. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
-# 32. Задайте последовательность чисел. Напишите программу, которая выведет список неповторяющихся элементов исходной последовательности.
-# 33. Задана натуральная степень k. Сформировать случайным образом список коэффициентов (значения от 0 до 100) многочлена и записать в файл многочлен степени k.
+# 33. Задана натуральная степень k. Сформировать случайным 
+# образом список коэффициентов (значения от 0 до 100)
+#  многочлена и записать в файл многочлен степени k.
 
 # Пример:
 # k=2 => 2x² + 4x + 5 = 0 или x² + 5 = 0 или 10x² = 0
-# 34. *Даны два файла, в каждом из которых находится запись многочлена. Задача - сформировать файл, содержащий сумму многочленов.
+
+# from random import randint
+# import itertools # принимает несколько итераций и возвращет одну последовательность
+
+# k = int(input('Введите натуральную степень k:'))
+# def get_koeff(k): 
+#     koeff = [randint(0, 100) for i in range (k + 1)]
+#     while koeff[0] == 0:
+#         koeff[0] = randint(1, 100)
+#         # коэфф. при старшей степени не должен быть равен 0 
+#     return koeff
+
+# def get_polynomial(k, koeff):
+#     var = ['x^']*(k-1) + ['x']
+#     # (k-1) - кол-во повторений [*x^]
+#     polynomial = [[a, b, c] for a, b, c  in itertools.zip_longest(koeff, var, range(k, 1, -1),
+#     fillvalue = '') if a !=0]
+#     # itertools.zip_longest(*iterables, fillvalue=None) fillvalue - пропущенные элементы
+#     print (polynomial)
+#     for x in polynomial:
+#         x.append(' + ')
+#     print(polynomial)
+#     polynomial = list(itertools.chain(*polynomial))
+#     # itertools.chain Создаёт итератор представляющий единой цепочкой элементы указанных объектов.
+#     polynomial[-1] = ' = 0'
+#     return "".join(map(str, polynomial)).replace(' 1*x',' x')
+
+
+# koeff = get_koeff(k)
+# polynom1 = get_polynomial(k, koeff)
+# print(polynom1)
+
+# with open('DZ _Pyt/Polynomial_1.txt', 'w') as data:
+#     data.write(polynom1)
+
+
+# 34. *Даны два файла, в каждом из которых находится запись многочлена.
+#  Задача - сформировать файл, содержащий сумму многочленов.
+from dataclasses import replace
+
+
+poly_1 = open('DZ _Pyt/Polynomial_1.txt').read().split('+')
+print((poly_1))
+s = list(zip(*[iter(poly_1)]*1))
+
+print(s)
+poly_2 = open('DZ _Pyt/Polynomial_2.txt').read().split('+')
+print(poly_2)
 
 # 2x² + 4x + 5 = 0 и x² + 5x + 3 = 0 => 3x² + 9x + 8 = 0
---
+++ DZ _Pyt/Zadacha_Les_5.py
@@ -0,0 +1,23 @@
+playing_field=['0','1','2','3','4','5','6','7','8'] #список с координатами игрового поля
+
+def printField(playing_field:list):# Функция выводящая отформатированное игровое поле
+    print(f'{playing_field[0]:^5}|{playing_field[1]:^5}|{playing_field[2]:^5}')
+    print('---------------')
+    print(f'{playing_field[3]:^5}|{playing_field[4]:^5}|{playing_field[5]:^5}')
+    print('---------------')
+    print(f'{playing_field[6]:^5}|{playing_field[7]:^5}|{playing_field[8]:^5}')
+
+print(printField(playing_field))
+
+def player_turn(playing_field:list): #функция ввода Х для игрока с проверкой на правильномть введения данных
+    while True:
+        x = int(input('Введите номер клетки для хода: '))
+        if(9>x>=0) and playing_field[x].isdigit():
+            playing_field[x]='X'
+            print(printField(playing_field))
+            # break
+        else:
+            print('Клетка занята, сделайте другой ход')
+
+print(player_turn(playing_field))
+
