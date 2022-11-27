0d3516a0f11a13f29605734c4ebedfd641771f8b
Firyuza_Lapteva <Gen.chel.74@gmail.com>
2022-11-28 02:14:51 +0500

    TG_calcul

--- TG_Bot/bot.py
+++ TG_Bot/bot.py
@@ -1,6 +1,6 @@
 from telegram.ext import Updater
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Updater, CommandHandler, CallbackContext, CallbackQueryHandler
 from telegram.ext import MessageHandler, Filters
 import logging
 import config
@@ -9,7 +9,6 @@ from logger import logger_bd
 
 # Создаем объект updater куда помещаем токен бота
 updater = Updater(token=config.token)
-response = requests.get(config.url).json() #декодирует данные с url адреса json
 # Создаем диспетчер
 dispatcher = updater.dispatcher
 # Функция логирования для отслеживания ошибок
@@ -31,14 +30,16 @@ def menu_command(update: Update, context: CallbackContext):
     logger_bd(update, context)
     update.message.reply_text(f" {update.effective_user.first_name}!"\
     "\n  Введи /menu, если хочешь посмотреть меня"
-    "\n  Введи /many <символьное обозначение валюты, например USD>, если хочешь узнать курс валюты")
+    "\n  Введи /money <символьное обозначение валюты, например USD>, если хочешь узнать курс валюты"
+    "\n Введи /calc_r, если хотите произвести операции с рациональными числами"
+    "\n Введи /calc_c, если хотите произвести операции с комплексными числами")
 updater.dispatcher.add_handler(CommandHandler('menu', menu_command))
 
 def process_coin_step (CharCode):
     response = requests.get(config.url).json()
     return response['Valute'][CharCode]['Name'], response['Valute'][CharCode]['Value'], response['Date']
 
-def many_command(update: Update, context: CallbackContext):
+def money_command(update: Update, context: CallbackContext):
     msg = update.message.text
     logger_bd(update, context)
     items = msg.upper().split() 
@@ -46,61 +47,172 @@ def many_command(update: Update, context: CallbackContext):
     items = items[1]
     context.bot.send_message (chat_id=update.effective_chat.id,
                             text=f'{process_coin_step(items)}')
-updater.dispatcher.add_handler(CommandHandler('many', many_command))
+updater.dispatcher.add_handler(CommandHandler('money', money_command))
 
-#
+def Summa(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()  # /2 + 2
+    x = float(items[1])
+    z = items[2]
+    y = float(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x + y}')
+updater.dispatcher.add_handler(CommandHandler('Summa', Summa))
 
+def Sub(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()  # /2 - 2
+    x = float(items[1])
+    z = items[2]
+    y = float(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x - y}')
+updater.dispatcher.add_handler(CommandHandler('Sub', Sub))
 
+def Multi(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()  # /2 - 2
+    x = float(items[1])
+    z = items[2]
+    y = float(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x * y}')
+updater.dispatcher.add_handler(CommandHandler('Multi', Multi))
 
-# обработка команды старт (создаем Inline клавиатуру)
-# def startCommand(update: Update, context: CallbackContext):
-#     buttonA = telegram.InlineKeyboardButton('Поздороваться', callback_data='buttonA')
-#     buttonB = telegram.InlineKeyboardButton('Посчитать сумму', callback_data='buttonB')
-#     buttonC = telegram.InlineKeyboardButton('Поменять список', callback_data='buttonC')
-#     buttonD = telegram.InlineKeyboardButton('Прогноз погоды', callback_data='buttonD')
-#     markup = telegram.InlineKeyboardMarkup(inline_keyboard=[[buttonA], [buttonB], [buttonC], [buttonD]])
-#     start_command_handler = CommandHandler('start', startCommand)
-#     CallbackQueryHandler(callback=callback, pattern=None, run_async=False)
-
-
-#     update.message.reply_text('Добрый день! Чтобы начать работу, выберите одно из возможных действий',
-#                               reply_markup=markup)
-#     return callback
-
-
-# # обработка нажатия клавиш клавиатуры
-# def callback(update: Update, context: CallbackContext):
-#     query = update.callback_query
-#     variant = query.data
-#     if variant == 'buttonA':
-#         query.answer()
-#         query.edit_message_text(text='Хотите поздороваться? Скажите привет!')
-
-#     if variant == 'buttonB':
-#         query.answer()
-#         query.edit_message_text(text='Введите "Сумма: 2 числа через пробел"')
-
-#     if variant == 'buttonC':
-#         query.answer()
-#         query.edit_message_text(text='Введите "Список: список через запятую"')
-
-#     if variant == 'buttonD':
-#         query.answer()
-#         query.edit_message_text(text='Напишите: "Город: свой город"')
-# # def echo(update: Update, context: CallbackContext):
-# #     logger_bd(update, context)
-# #     msg = update.message.text 
-# #     context.bot.send_message(chat_id=update.effective_chat.id, text= msg)
-
-# # # обработчик текстовых сообщений
-# # echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
-# # dispatcher.add_handler(echo_handler)# функция обработки текстовых сообщений
+def Div(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()  # /2 - 2
+    x = float(items[1])
+    z = items[2]
+    y = float(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x / y}')
+updater.dispatcher.add_handler(CommandHandler('Div', Div))
 
 
-# функция обработки не распознных команд
+
+# обработка команды калькулятор - рациональные числа (создаем Inline клавиатуру)
+def calc_r(update: Update, context: CallbackContext):
+    logger_bd(update, context)
+    buttonA = InlineKeyboardButton('Summa', callback_data='buttonA')
+    buttonB = InlineKeyboardButton('Sub', callback_data='buttonB')
+    buttonC = InlineKeyboardButton('Multi', callback_data='buttonC')
+    buttonD = InlineKeyboardButton('Div', callback_data='buttonD')
+    markup = InlineKeyboardMarkup(inline_keyboard=[[buttonA], [buttonB], [buttonC], [buttonD]])
+    updater.dispatcher.add_handler(CallbackQueryHandler(callback=callback1, pattern=None, run_async=False)
+)
+    update.message.reply_text('Добрый день! Чтобы начать работу, выберите одно из' 
+                                ' возможных действий', reply_markup=markup)
+    return callback1
+updater.dispatcher.add_handler(CommandHandler('calc_r', calc_r))
+
+#  обработка нажатия клавиш клавиатуры
+def callback1(update: Update, context: CallbackContext):
+    query = update.callback_query
+    variant = query.data
+    if variant == 'buttonA':
+        query.answer()
+        query.edit_message_text(text='Введите "/Summa 2 + 2"')
+
+    if variant == 'buttonB':
+        query.answer()
+        query.edit_message_text(text='Введите" /Sub 4 - 2 "')
+
+    if variant == 'buttonC':
+        query.answer()
+        query.edit_message_text(text='Введите" /Multi  4 - 2 "')
+
+    if variant == 'buttonD':
+        query.answer()
+        query.edit_message_text(text='Введите" /Div  4 - 2 "')
+
+def SummaС(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    items = msg.split()
+    x = complex(items[1])
+    z = items[2]
+    y = complex(items[3])
+
+    update.message.reply_text(f'{x} {z} {y} = {x + y}')
+updater.dispatcher.add_handler(CommandHandler('SummaC', SummaС))
+
+def SubC(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()
+    x = complex(items[1])
+    z = items[2]
+    y = complex(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x - y}')
+updater.dispatcher.add_handler(CommandHandler('SubC', SubC))
+
+def MultiC(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()
+    x = complex(items[1])
+    z = items[2]
+    y = complex(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x * y}')
+updater.dispatcher.add_handler(CommandHandler('MultiC', MultiC))
+
+def DivC(update: Update, context: CallbackContext):
+    msg = update.message.text
+    logger_bd(update, context)
+    print(msg)
+    items = msg.split()
+    x = complex(items[1])
+    z = items[2]
+    y = complex(items[3])
+    update.message.reply_text(f'{x} {z} {y} = {x / y}')
+updater.dispatcher.add_handler(CommandHandler('DivC', DivC))
+
+# обработка команды калькулятор - комплекные числа (создаем Inline клавиатуру)
+def calc_c(update: Update, context: CallbackContext):
+    logger_bd(update, context)
+    buttonE = InlineKeyboardButton('SummaС', callback_data='buttonE')
+    buttonF = InlineKeyboardButton('SubС', callback_data='buttonF')
+    buttonG = InlineKeyboardButton('MultiС', callback_data='buttonG')
+    buttonS = InlineKeyboardButton('DivС', callback_data='buttonS')
+    markup = InlineKeyboardMarkup(inline_keyboard=[[buttonE], [buttonF], [buttonG], [buttonS]])
+    updater.dispatcher.add_handler(CallbackQueryHandler(callback=callback2, pattern=None, run_async=False)
+)
+    update.message.reply_text('Добрый день! Чтобы начать работу, выберите одно из' 
+                                ' возможных действий', reply_markup=markup)
+    return callback2
+updater.dispatcher.add_handler(CommandHandler('calc_c', calc_c))
+
+#  обработка нажатия клавиш клавиатуры
+def callback2(update: Update, context: CallbackContext):
+    query = update.callback_query
+    variant = query.data
+    if variant == 'buttonE':
+        query.answer()
+        query.edit_message_text(text='Введите /SummaC (1+2j) + (1+2j)')
+
+    if variant == 'buttonF':
+        query.answer()
+        query.edit_message_text(text='Введите  /SubC (1+2j) - (1+2j)')
+
+    if variant == 'buttonG':
+        query.answer()
+        query.edit_message_text(text='Введите /MultiC (1+2j) * (1+2j)')
+
+    if variant == 'buttonS':
+        query.answer()
+        query.edit_message_text(text='Введите /DivC (1+2j) / (1+2j)')
+
+# функция обработки не распознанных команд
 def unknown(update, context):
     context.bot.send_message(chat_id=update.effective_chat.id, 
-                             text="Sorry, I didn't understand that command.")   
+                             text="Извините, я не знаю такой команды")   
 
 # обработчик не распознанных команд
 unknown_handler = MessageHandler(Filters.command, unknown)
